28d549e7536bc40b6e415d2dea62541da88b46d6
wtm99 <willmayes1999@gmail.com>
2022-02-07 16:11:31 -0500

    Fixed Typo

--- BLUESPAWN-win-client/test/Check/Hunt T1546/Fixed/huntT1546-sub002-test001.ps1
+++ BLUESPAWN-win-client/test/Check/Hunt T1546/Fixed/huntT1546-sub002-test001.ps1
@@ -3,7 +3,7 @@ if (Test-Path "HKCU:\Control Panel\Desktop"){
     try {
         $value = Get-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "SCRNSAVE.exe" -ErrorAction SilentlyContinue
         if ($value.'SCRNSAVE.exe' -eq "C:\temp\evil.exe"){
-            Write-Output "HuntT1546-Sub02-Test001: Registry value not removed"
+            Write-Output "HuntT1546-Sub002-Test001: Registry value not removed"
             $passed = $false;
         }
     } 

162ec8d64c2ea95201aa5cc03a79dda49bc59936
wtm99 <willmayes1999@gmail.com>
2022-02-23 11:27:29 -0500

    Added t1546 subtechnique 8

---
+++ BLUESPAWN-win-client/test/Check/HuntT1546/Fixed/huntT1546-sub002-test001.ps1
@@ -0,0 +1,14 @@
+$passed = $true
+if (Test-Path "HKCU:\Control Panel\Desktop"){
+    try {
+        $value = Get-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "SCRNSAVE.exe" -ErrorAction SilentlyContinue
+        if ($value.'SCRNSAVE.exe' -eq "C:\temp\evil.exe"){
+            Write-Output "HuntT1546-Sub002-Test001: Registry value not removed"
+            $passed = $false;
+        }
+    } 
+    catch{
+        Write-Output "HI"
+    }
+}
+return $passed
---
+++ BLUESPAWN-win-client/test/Clean/HuntT1546/huntT1546-sub008-test001.bat
@@ -0,0 +1,2 @@
+@echo off
+reg delete "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\sethc.exe" /f

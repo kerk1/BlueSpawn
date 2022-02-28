f3572c9887cc23e0d2e8c37fedbb98661ef2ccfe
wtm99 <willmayes1999@gmail.com>
2022-02-28 16:25:56 -0500

    Fixed bug in tests

--- BLUESPAWN-win-client/test/Check/HuntT1546/Fixed/huntT1546-sub009-test001.ps1
+++ BLUESPAWN-win-client/test/Check/HuntT1546/Fixed/huntT1546-sub009-test001.ps1
@@ -2,7 +2,7 @@ $passed = $true
 if (Test-Path "HKLM:\System\CurrentControlSet\Control\Session Manager"){
     try {
         $value = Get-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Session Manager" -Name "AppCertDLLs" -ErrorAction SilentlyContinue
-        if ($value.evil -eq "C:\Temp\evil.dll"){
+        if ($value.appcertdlls -eq "C:\Temp\evil.dll"){
             Write-Output "Hunt-T1546-Sub009-Test001: Registry value not removed."
             $passed = $false;
         }
--- BLUESPAWN-win-client/test/Check/HuntT1546/Fixed/huntT1546-sub010-test002.ps1
+++ BLUESPAWN-win-client/test/Check/HuntT1546/Fixed/huntT1546-sub010-test002.ps1
@@ -1,8 +1,8 @@
 $passed = $true
 if (Test-Path "HKLM:\Software\Wow6432Node\Windows NT\CurrentVersion\Windows"){
     try {
-        $value = Get-ItemProperty -Path "HKLM:\Software\Wow6432Node\Windows NT\CurrentVersion\Windowss" -Name "AppInit_DLLs" -ErrorAction SilentlyContinue
-        if ($value.evil -eq "C:\Temp\evil.dll"){
+        $value = Get-ItemProperty -Path "HKLM:\Software\Wow6432Node\Windows NT\CurrentVersion\Windows" -Name "AppInit_DLLs" -ErrorAction SilentlyContinue
+        if ($value.appinit_dlls -eq "C:\Temp\evil.dll"){
             Write-Output "Hunt-T1546-Sub010-Test002: Registry value not removed."
             $passed = $false;
         }

6f01fe5cce2e2b07012043b297338a035a99074c
wtm99 <willmayes1999@gmail.com>
2022-03-21 20:34:53 -0400

    Added checking for if the attack was properly logged

--- BLUESPAWN-win-client/test/RunSystemTests.ps1
+++ BLUESPAWN-win-client/test/RunSystemTests.ps1
@@ -1,13 +1,21 @@
-﻿.\RunAttacks.ps1
+﻿del *.json
+.\RunAttacks.ps1
 #Run BLUESPAWN args[0] is path to BLUESPAWN
 $yes = "y`n"*1000
-$job = Start-Job -ScriptBlock {
-    &$args[0] --hunt --log=console,json -a Intensive --hunts T1546 -r "remove-value,delete-file"
+$job = Start-Job -Init ([ScriptBlock]::Create("Set-Location '$pwd'")) -ScriptBlock {
+    &$args[0] --hunt --log=json -a Intensive --hunts T1037 -r "remove-value,delete-file"
 } -ArgumentList $args[0] -InputObject $yes
+#&$args[0] --hunt --log=xml -a Intensive --hunts T1546 -r "remove-value,delete-file"
 Wait-Job $job -Timeout 15
 Receive-Job $job
 Stop-Job $job
-#$yes | C:\Users\Admin\Downloads\BLUESPAWN-client-x64.exe --hunt --log=console,xml -a Intensive --hunts T1546 --react remove-value,carve-memory,delete-file
-$fullyPassed = .\CheckFixed.ps1
+$fixedPassed = .\CheckFixed.ps1
+$checkPassed = .\CheckCaught.ps1
 .\CleanUp.ps1
-return $fullyPassed
+Write-Output "Attacks Remediated:"
+Write-Output $fixedPassed
+$fixedPassed = ($fixedPassed -split "`n") | Select-Object -Last 1
+Write-Output "Attacks Caught:"
+Write-Output $checkPassed
+$checkPassed = ($checkPassed -split "`n") | Select-Object -Last 1
+return ($fixedPassed -eq "True") -band ($checkPassed -eq "True")
